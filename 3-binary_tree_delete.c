2d76f20ae75e1a0e0dcbc9706471d7dc37a14011
Dylan Serigba <12297@holbertonstudents.com>
2025-12-10 14:02:52 +0100

    Deleting an entire binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,25 +1,15 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * binary_tree_delete - Deletes an entire binary tree
- * @tree: Pointer to the root node of the tree to delete
- *
- * Description: Uses post-order traversal to free memory.
- * Does nothing if tree is NULL.
+ * binary_tree_delete - Deletes a binary tree
+ * @tree: the tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    /* Condition d'arrêt de la récursion */
-    if (tree == NULL)
-        return;
+	if (!tree)
+		return;
 
-    /* 1. On supprime d'abord tout le sous-arbre gauche */
-    binary_tree_delete(tree->left);
-
-    /* 2. On supprime ensuite tout le sous-arbre droit */
-    binary_tree_delete(tree->right);
-
-    /* 3. Enfin, on supprime le nœud actuel */
-    free(tree);
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
